f3059a3f3b8e9864c148312322dcfd532ce09442
Annany Vishwakarma <annany.21211@knit.ac.in>
2023-07-28 11:38:07 +0530

    changes made

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,24 +1,24 @@
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { SelectedPage } from '../../shared/types';
 
 const childVariant = {
 	hidden: { opacity: 0, scale: 0.9 },
-	visible: { opacity: 1, scale:1 }
+	visible: { opacity: 1, scale: 1 }
 }
 
 type Props = {
-	icon:JSX.Element;
-	title:string;
-	description:string;
-	setSelectedPage:(value: SelectedPage) => void
+	icon: JSX.Element;
+	title: string;
+	description: string;
+	setSelectedPage: (value: SelectedPage) => void
 }
 
-export default function Benefit({icon, title, description, setSelectedPage}: Props){
-	return(
-		<motion.div 
+export default function Benefit({ icon, title, description, setSelectedPage }: Props) {
+	return (
+		<motion.div
 			variants={childVariant}
-		 	className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
+			className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
 			<div className='mb-4 flex justify-center'>
 				<div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
 					{icon}
@@ -30,9 +30,9 @@ export default function Benefit({icon, title, description, setSelectedPage}: Pro
 				className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
 				onClick={() => setSelectedPage(SelectedPage.ContactUs)}
 				href={`#${SelectedPage.ContactUs}`}
-				>
-				  <p>Learn More</p>
-				</AnchorLink>
+			>
+				<p>Learn More</p>
+			</AnchorLink>
 		</motion.div>
-		)
+	)
 }
